c8149b713d8955212f7921cb9bf2656355780bef
ꜱᴏᴜᴛʜᴋɪʟʟᴀ <91430346+GzForeign@users.noreply.github.com>
2022-06-07 01:58:22 +0700

    Update logger.py

--- Music/MusicUtilities/helpers/logger.py
+++ Music/MusicUtilities/helpers/logger.py
@@ -11,12 +11,12 @@ async def LOG_CHAT(message, what):
     user_name = message.from_user.first_name
     mention = "["+user_name+"](tg://user?id="+str(user_id)+")" 
     logger_text = f"""
-__**New {what}**__
+__**Aww {what}**__
 
 **Chat:** {message.chat.title} [`{message.chat.id}`]
-**User:** {mention}
-**Username:** @{message.from_user.username}
-**User ID:** `{message.from_user.id}`
+**Pengguna:** {mention}
+**Nama Pengguna:** @{message.from_user.username}
+**ID Pengguna:** `{message.from_user.id}`
 **Chat Link:** {chatusername}
 **Query:** {message.text}"""
     await ASS_ACC.send_message(LOG_GROUP_ID, f"{logger_text}", disable_web_page_preview=True)
